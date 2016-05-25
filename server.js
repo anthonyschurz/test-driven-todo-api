0dcc3ab4d5538158190e98ca53d9cdf2965fe093
anthonyschurz <anthony.schurz@gmail.com>
2016-05-25 16:38:21 -0700

    makes changes

--- server.js
+++ server.js
@@ -44,39 +44,70 @@ app.get('/', function homepage(req, res) {
  */
 
 app.get('/api/todos/search', function search(req, res) {
-  /* This endpoint responds with the search results from the
-   * query in the request. COMPLETE THIS ENDPOINT LAST.
-   */
+
+   var term = req.query.task;
+
+   for (i = 0; i < length.todos; i++) {
+      if (term == todos[i].task) {
+        res.json(todos[i]);
+      } else {
+        console.log("we couldn't find any results")
+      }
+   };
 });
+
+
 
 app.get('/api/todos', function index(req, res) {
    res.json({ todos: todos });
 });
 
 app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
-   * and respond with the newly created todo.
-   */
+  var newTodo = req.body;
+
+  if (todos.length > 0) {
+    newTodo._id = todos[todos.length - 1]._id + 1;
+  } else {
+    newTodo._id = 1;
+  }
+
+  todos.push(newTodo);
+  res.json(newTodo);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
-   * id specified in the route parameter (:id)
-   */
+  for (var i = 0; i < todos.length; i++) {
+    if (req.params.id == todos[i]._id) {
+      res.json(todos[i]);
+    }
+  }
 });
 
 app.put('/api/todos/:id', function update(req, res) {
-  /* This endpoint will update a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with the newly updated todo.
-   */
+   var updID = req.params.id;
+   var updTodo = req.body;
+
+   for (var i = 0; i < todos.length; i++) {
+     if (updID == todos[i]._id) {
+       updTodo._id = todos[i]._id;
+       todos[i] = updTodo;
+     } else {
+       console.log("ID not found");
+     }
+   }
+res.json(updTodo);
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
-  /* This endpoint will delete a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with success.
-   */
+  var destID = req.params.id;
+  for (var i = 0; i < todos.length; i++) {
+    if (destID == todos[i]._id) {
+      todos.splice(i, 1);
+    } else {
+      console.log("ID not found");
+    }
+  }
+  res.json({ todos: todos })
 });
 
 /**********
